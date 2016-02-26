8418f4f051c37ced14b6a5ce0a3a05b75b82df8c
daeinpark <farloking@gmail.com>
2016-02-26 23:22:51 +0900

    1

--- eyagi/settings.py
+++ eyagi/settings.py
@@ -111,13 +111,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/app/static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 ADMIN_MEDIA_PREFIX = 'static/admin'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
-    'app/static/',
+    '/static/',
 )
 #login
 LOGIN_REDIRECT_URL='/index/'
